094e0b28cc2795d8252fe5b9beee35d4b7c371b9
grimmer <ron.grimmer@fhws.de>
2018-01-15 10:54:54 +0100

    updated my new todo controller

--- api/controllers/todosCtrl.js
+++ api/controllers/todosCtrl.js
@@ -5,3 +5,9 @@ exports.findTasks = function(req, res) {
     // var tasks = [{task: "todo 1"}, {title: "todo 2"}];
     res.json(tasks);
 };
+
+exports.createTask = function(req, res) {
+
+    console.log('Hello from our todos service controller. awesome.');
+    res.send('Add a book');
+};
--- api/routes/todosRoutes.js
+++ api/routes/todosRoutes.js
@@ -2,7 +2,11 @@
 module.exports = function(app) {
     var todosCtrl = require('../controllers/todosCtrl');
 
-    // just the get tasks route
+    // C - REATE Operation
+    app.route('/create-new-task').post(todosCtrl.createTask);
+
+    // R - EAD Operation
     app.route('/tasks').get(todosCtrl.findTasks);
+
 
 };
